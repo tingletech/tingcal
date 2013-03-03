a1917ea27bdb53efd043c14363ef3562285b2bc4
Brian Tingle <brian.tingle.cdlib.org@gmail.com>
2013-03-03 11:36:28 -0800

    add zodiac support

--- README.md
+++ README.md
@@ -3,11 +3,11 @@
 Playing around with a funky calendar scheme
 
 sample calendar 
- * [pdf](http://tingletech.github.com/tingcal/2013.pdf)
- * [unicode text](http://tingletech.github.com/tingcal/2013.txt)
+ * [pdf](http://tingletech.tumblr.com/tingcal/2013.zodiac.pdf)
+ * [unicode text](http://tingletech.tumblr.com/tingcal/2013.zodiac.txt)
 
 ```
-usage: tingcal.py [-h] startmonth endmonth
+usage: tingcal.py [-h] [--zodiac] startmonth endmonth
 
 create a calendar
 
@@ -17,46 +17,43 @@ positional arguments:
 
 optional arguments:
   -h, --help  show this help message and exit
-```
+  --zodiac    print zodiac signs```
 
 ## example
 ```
-./tingcal.py 2013-03 2013-06
+./tingcal.py 2013-03 2013-06 --zodiac 
 YYYY-Www  Mo Tu We Th Fr Sa Su     
 2013-W09  25 26 27 28  ③  2  3     
 2013-W10   4  5  6  7  8  9 10     
 2013-W11  11 12 13 14 15 16 17     
-2013-W12  18 19 20 21 22 23 24     
+2013-W12  18 19  ♈ 21 22 23 24     
 2013-W13  25 26 27 28 29 30 31     
 2013-W14   ④  2  3  4  5  6  7     
 2013-W15   8  9 10 11 12 13 14     
-2013-W16  15 16 17 18 19 20 21     
+2013-W16  15 16 17 18 19  ♉ 21     
 2013-W17  22 23 24 25 26 27 28     
 2013-W18  29 30  ⑤  2  3  4  5     
 2013-W19   6  7  8  9 10 11 12     
 2013-W20  13 14 15 16 17 18 19     
-2013-W21  20 21 22 23 24 25 26     
+2013-W21  20  ♊ 22 23 24 25 26     
 2013-W22  27 28 29 30 31  ⑥  2     
 2013-W23   3  4  5  6  7  8  9     
 2013-W24  10 11 12 13 14 15 16     
-2013-W25  17 18 19 20 21 22 23     
+2013-W25  17 18 19 20  ♋ 22 23     
 2013-W26  24 25 26 27 28 29 30     
 ```
 
 ## Why?  What?
 
-This is how I like to see a calendar.  Why should weeks split over
-months?  I used to make these on graph paper when I was in high
-school and college (sans the ISO weeks, but with the week of the
-term), and I just relaized the other day I could use "enclosed
-alphanumerics" from unicode to mark the start of the months with a
-calendar from the unix command `cal`.  
+This is how I like to see a calendar.  
 
-I added in the [ISO 8601 calendar](http://www.staff.science.uu.nl/~gent0113/calendar/isocalendar.htm) weeks because of the general irony
-of [xkcd.com/1179/](http://xkcd.com/1179/) and a discussion about it on facebook.
+I added in the [ISO 8601 calendar](http://www.staff.science.uu.nl/~gent0113/calendar/isocalendar.htm) weeks because of 
+of [xkcd.com/1179/](http://xkcd.com/1179/).
 There are also YYYY-Www-dd and YYYY-ddd that use numbers for the
 day of the week of the day of the year in addition to YYYY-MM-dd.
 
-I might add the day of the year that each week starts on.
+I added in the zodiac signs for entertianment purposes only.
 
-I might add zodiac signs / full moons options.
+I might add the ISO calendar day of the year that each week starts on (for YYYY-ddd).
+
+I might add full moons option (it would have to be exclusive to the zodiac option)
--- tingcal.py
+++ tingcal.py
@@ -15,10 +15,10 @@ from __future__ import unicode_literals
 import argparse		# http://docs.python.org/2.7/library/argparse.html
 import sys		# http://docs.python.org/2.7/library/sys.html
 import calendar 	# http://docs.python.org/2/library/calendar.html 
-import datetime         # 
+import datetime         # http://docs.python.org/2/library/datetime.html#date-objects
 import pprint
 
-def tingcal(start, end):
+def tingcal(start, end, zodiac):
     # http://stackoverflow.com/a/4040204/1763984
     start_month=start.month
     end_months=(end.year-start.year)*12 + end.month+1
@@ -41,26 +41,52 @@ def tingcal(start, end):
             last_day_of_week = week[6]
             iso_week = first_day_of_week.isocalendar()[1]
             iso_year = first_day_of_week.isocalendar()[0]
+            # in order to prevent double weeks at the boundry between months
             # show only weeks where:
             if ( first_day_of_week.month == last_day_of_week.month 
-                          # all days in same month
+                          # ↖ all days in same month
                    or first_week
-                          # first week of month
+                          # ↖ first week of month
                    or ( year == end.year and month == end.month)
-                          # last month of calendar
+                          # ↖ last month of calendar
             ):
+                # print the out ISO 8601 calendar week
+                #           YYYY-Www
                 out = out + u'{}-W{:02d} '.format(iso_year, iso_week)
-                # day below is a datetime.date http://docs.python.org/2/library/datetime.html#date-objects
+                # day below is a datetime.date 
                 for day in week:
+                    # check if the zodiac sign changes today
+                    if zodiac:
+                        zodiac_code = zodiac_start(day.month, day.day)
                     # the first day of the month is a special day
                     if day.day == 1:
                         num = unichr(start_enclosed_alphanumeric_range + day.month - 1)
                         out = out + u'  ' + num
+                    elif zodiac_code:
+                        out = out + u'  ' + zodiac_code
                     else:
                         out = out + u'{:>3}'.format(day.day)
                 out = out + u"     \n"
                 first_week = False
     print out.encode("utf-8")
+
+def zodiac_start(month, day):
+    signs = { 
+              (  1, 20 ): u"\u2652",
+              (  2, 18 ): u"\u2653",
+              (  3, 20 ): u"\u2648",
+              (  4, 20 ): u"\u2649",
+              (  5, 21 ): u"\u264A",
+              (  6, 21 ): u"\u264B",
+              (  7, 22 ): u"\u264C",
+              (  8, 23 ): u"\u264D",
+              (  9, 23 ): u"\u264E",
+              ( 10, 23 ): u"\u264F",
+              ( 11, 22 ): u"\u2650",
+              ( 12, 22 ): u"\u2651",
+            }
+    if (month,day) in signs:
+        return signs[(month,day)]
 
 # main() idiom for importing into REPL for debugging 
 def main(argv=None):
@@ -70,13 +96,14 @@ def main(argv=None):
     parser = argparse.ArgumentParser(description='create a calendar')
     parser.add_argument('startmonth', nargs=1, type=datearg, help='start month YYYY-MM') 
     parser.add_argument('endmonth', nargs=1, type=datearg, help='end month YYYY-MM')
+    parser.add_argument('--zodiac', action='store_const', const=True, help='print zodiac signs')
     # options for which set of enclosed characters are used
     # parser.add_argument('--start-enclosed-range' default u'①' hex 2460 decimal 9312
     # parser.add_argument('--start-zodiac-range' default 
     if argv is None:
         argv = parser.parse_args()
 
-    tingcal(argv.startmonth[0], argv.endmonth[0])
+    tingcal(argv.startmonth[0], argv.endmonth[0], argv.zodiac)
 
 if __name__ == "__main__":
     sys.exit(main())
